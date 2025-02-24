68a3012048cdff046058164c1820c69ea3e61b33
Gmailvicente04 <vicente.oswald@gmail.com>
2025-02-23 23:11:00 -0500

    "Task 2 - Created Order Class"

--- cc_10.js
+++ cc_10.js
@@ -20,3 +20,22 @@ const prod1 = new Product("Laptop", 101, 1200, 10);
 console.log(prod1.getDetails());
 prod1.updateStock(3);
 console.log(prod1.getDetails());
+
+// Task 2: Creating Order Class
+class Order {
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+        this.totalPrice = product.price * quantity;
+        product.updateStock(quantity);
+    }
+    
+    getOrderDetails() {
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.totalPrice}`;
+    }
+}
+
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails());
+console.log(prod1.getDetails());
